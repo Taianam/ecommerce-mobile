859bb261e02cb9f8d3e4814895131453175785fe
Taianam <taiana.monteirof@gmail.com>
2021-07-13 19:06:23 -0300

    Cadastro de produtos

---
+++ src/components/data/index.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import {View, Button, Platform} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+export const Data = ({inserirDataCadastro}) => {
+  const [date, setDate] = useState(new Date);
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+    inserirDataCadastro(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  return (
+    <View>
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+    </View>
+  );
+};
--- src/screens/produto/cadastrar/index.jsx
+++ src/screens/produto/cadastrar/index.jsx
@@ -1,21 +1,114 @@
-import React from 'react';
-import { View, TextInput } from 'react-native';
+import api from "../../../service/api";
+import React, { useEffect, useState, useRef } from "react";
+import { View, TextInput, TouchableOpacity, Text } from "react-native";
+import { Data } from "../../../components/data";
+import { Picker } from "@react-native-picker/picker";
 
-export function Cadastrar(){
-    const [ nome, setNome  ] = useState();
-    const [ descricao, setDescricao ] = useState();
-    const [ imagem, setImagem ] = useState();
-    const [ estoque, setEstoque ] = useState();
-    const [ preco, setPreco ] = useState();
-    const [ dataCadastro, setDataCadastro ] = useState(new Date().getDate());
+export function Cadastrar() {
+  const [nome, setNome] = useState();
+  const [descricao, setDescricao] = useState();
+  const [imagem, setImagem] = useState();
+  const [estoque, setEstoque] = useState();
+  const [preco, setPreco] = useState();
+  const [dataCadastro, setDataCadastro] = useState();
+  const [categoria, setCategoria] = useState([]);
+  const [categoriaId, setCategoriaId] = useState([])
 
-    return(
-        <View>
-            <TextInput value={nome} onChangeText={setNome}/>
-            <TextInput value={descricao} onChangeText={setDescricao}/>
-            <TextInput value={imagem} onChangeText={setImagem}/>
-            <TextInput value={estoque} onChangeText={setEstoque}/>
-            <TextInput value={preco} onChangeText={setPreco}/>
-        </View>
-    )
+
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
+  // Função que recebe uma data do compenente DATA e inseri no estado.
+  function inserirDataCadastro(data) {
+    const dataFormatada = formatarData(data);
+    setDataCadastro(dataFormatada);
+  }
+
+  // Função que vai tratar a data de dataCadastro
+  function formatarData(data) {
+    return String(data.toISOString().slice(0, 10));
+  }
+
+  // Função que vai cadastrar os produtos
+  const cadastrarProduto = async () => {
+    
+    try {
+      const novoProduto = {
+        nome: nome,
+        descricao: descricao,
+        imagem: imagem,
+        estoque: estoque,
+        dataDeCadastro: dataCadastro,
+        preco: preco,
+      };
+   
+       await api.post(`produtos/${categoriaId}`, novoProduto);
+     } catch (error) {
+       console.log(error);
+     }
+  };
+
+  useEffect(() => {
+    obterCategorias();
+  }, []);
+  //Função que busca as categorias
+  const obterCategorias = async () => {
+    try {
+      const { data } = await api.get("categorias");
+      console.log(data);
+      setCategoria(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  return (
+    <View>
+      <TextInput
+        style={{ backgroundColor: "violet", margin: 20 }}
+        value={nome}
+        onChangeText={setNome}
+        placeholder="Nome"
+      />
+      <TextInput
+        style={{ backgroundColor: "violet", margin: 20 }}
+        value={descricao}
+        onChangeText={setDescricao}
+        placeholder="Descrição"
+      />
+      <TextInput
+        style={{ backgroundColor: "violet", margin: 20 }}
+        value={imagem}
+        onChangeText={setImagem}
+        placeholder="Imagem"
+      />
+      <TextInput
+        style={{ backgroundColor: "violet", margin: 20 }}
+        value={estoque}
+        onChangeText={setEstoque}
+        placeholder="Estoque"
+      />
+      <TextInput
+        style={{ backgroundColor: "violet", margin: 20 }}
+        value={preco}
+        onChangeText={setPreco}
+        placeholder="Preço"
+      />
+
+      <Picker
+        selectedValue={categoriaId}
+        onValueChange={(itemValue) => setCategoriaId(itemValue)}
+        
+      >
+        {categoria.map((c) => (
+          <Picker.Item label={c.nome} value={c.id} />
+        ))}
+      </Picker>
+
+      <Data inserirDataCadastro={inserirDataCadastro} />
+      <TouchableOpacity onPress={cadastrarProduto}>
+        <Text>Cadastrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
