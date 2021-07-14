1d39c7c146b7862be523bfaecaa8f1af8f29112c
LarissaCouto14 <81425436+LarissaCouto14@users.noreply.github.com>
2021-07-14 16:44:40 -0300

    alterações

--- src/screens/home/style.jsx
+++ src/screens/home/style.jsx
@@ -1,22 +1,21 @@
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
-	fundo: {
-		flex: 1,
-		backgroundColor: "#ecaba7",
-		padding: 10,
-	},
-
-	titulo: {
-		color: "#ffff",
-		textAlign: "center",
-		fontSize: 30,
-	},
-	img: {
-		width: "100%",
-		height: "20%",
-	},
-	input: {
-		backgroundColor: "white",
-	},
+    fundo: {
+        flex: 1,
+        backgroundColor: "#ecaba7",
+        padding: 10,
+    },
+    titulo: {
+        color: "#ffff",
+        textAlign: "center",
+        fontSize: 30,
+    },
+    img: {
+        width: "100%",
+        height: "40%",
+    },
+    input: {
+        backgroundColor: "white",
+    }
 });
