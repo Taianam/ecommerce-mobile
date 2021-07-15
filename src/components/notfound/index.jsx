bc27f02dcc3f507bc7bc2f3545a0633f73294096
zsazsaspeck <giovannemenezeslopes@hotmail.com>
2021-07-15 16:16:03 -0300

    I miss brunette

--- src/components/notfound/index.jsx
+++ src/components/notfound/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import { View, Text, Image, ImageBackground } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 import { style } from "./styles";
 
+const candycrying = { uri: "https://i.ibb.co/gSwr8YQ/Inside-Out.png" }
+const fundo = { uri: "https://i.ibb.co/wrPprnF/2797738.jpg"}
 export function NotFound(){
     return(
         <View style={style.container}>
-            <FontAwesome name="hand-stop-o" size={50} color="red" />
-            <Text style={style.titulo}> Ops ! Nenhum resultado encontrado </Text>
+            <ImageBackground source={fundo} style={style.fundo}>
+            <View>
+            <Text style={style.texto}>Ops! Nenhum doce encontrado. 
+                <FontAwesome5 name="sad-cry" size={24} color="grey" />
+                 </Text>
+            <Image source={candycrying} style={style.cry}/>
+            </View>
+            
+            </ImageBackground>
         </View>
     )
 }
--- src/components/notfound/styles.jsx
+++ src/components/notfound/styles.jsx
@@ -2,13 +2,29 @@ import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
     container:{
+        flex:1,
+    },
+
+    cry: {
+        width:300,
+        height:300,
         alignItems: 'center',
         justifyContent: 'center',
-        height: 250,
     },
      titulo:{
          fontSize: 24,
          fontWeight: 'bold',
          textAlign: 'center'
+     },
+     fundo:{
+         flex:1,
+         opacity:0.9
+     },
+     texto:{
+         marginBottom:30,
+         fontSize:25,
+         alignItems: 'center',
+         justifyContent: 'center',
+         fontWeight: 'bold'
      }
 });
